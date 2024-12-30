7efeef760d6cfe4a072c7ea73850154e04b839d3
Arif Emre Ankara <arifemreankara@192.168.1.189>
2024-12-30 16:03:50 +0300

    fix

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-      host: 'localhost',
+      host: '127.0.0.1',
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      port: 3306,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0
